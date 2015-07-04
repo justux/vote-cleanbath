35e7ff39ce76378ab0854ce665c599bd1bd5a9ce
Alberto Giustino <alberto.giustino@artexe.com>
2015-07-04 16:26:31 +0200

    Progetto base: login, pannello generale, pannello privato e pubblico

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngMockE2E'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,77 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(function ($stateProvider, $urlRouterProvider, USER_ROLES) {
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('main', {
+    url: '/',
+    abstract: true,
+    templateUrl: 'templates/main.html'
+  })
+  .state('main.dash', {
+    url: 'main/dash',
+    views: {
+        'dash-tab': {
+          templateUrl: 'templates/dashboard.html',
+          controller: 'DashCtrl'
+        }
+    }
+  })
+  .state('main.public', {
+    url: 'main/public',
+    views: {
+        'public-tab': {
+          templateUrl: 'templates/public.html'
+        }
+    }
+  })
+  .state('main.admin', {
+    url: 'main/admin',
+    views: {
+        'admin-tab': {
+          templateUrl: 'templates/admin.html'
+        }
+    },
+    data: {
+      authorizedRoles: [USER_ROLES.admin]
+    }
+  });
+  $urlRouterProvider.otherwise('/main/dash');
+})
+
+.run(function($httpBackend){
+  $httpBackend.whenGET('http://localhost:8100/valid')
+        .respond({message: 'This is my valid response!'});
+  $httpBackend.whenGET('http://localhost:8100/notauthenticated')
+        .respond(401, {message: "Not Authenticated"});
+  $httpBackend.whenGET('http://localhost:8100/notauthorized')
+        .respond(403, {message: "Not Authorized"});
+ 
+  $httpBackend.whenGET(/templates\/\w+.*/).passThrough();
+ })
+
+ .run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+ 
+    if ('data' in next && 'authorizedRoles' in next.data) {
+      var authorizedRoles = next.data.authorizedRoles;
+      if (!AuthService.isAuthorized(authorizedRoles)) {
+        event.preventDefault();
+        $state.go($state.current, {}, {reload: true});
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      }
+    }
+ 
+    if (!AuthService.isAuthenticated()) {
+      if (next.name !== 'login') {
+        event.preventDefault();
+        $state.go('login');
+      }
+    }
+  });
+});
